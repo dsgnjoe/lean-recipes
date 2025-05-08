c87e9ab22851e3d35fbd3bea4311e4cc88a1259c
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-08 13:00:13 +0000

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Recipes from './pages/Recipes'
+import SignUp from './pages/SignUp'
+
 function App() {
   return (
     <>
-      <h1 className='text-red-600 font-bold underline'>Hello Word</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/recipes' element={<Recipes />} />
+            <Route path='/sign-up' element={<SignUp />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
