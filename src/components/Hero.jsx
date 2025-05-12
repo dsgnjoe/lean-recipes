d3dc71d4dbc85b26622739eaf25ff4361589bf4f
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-12 23:40:28 +0000

    hero section update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,27 +3,25 @@ import { IoArrowForwardCircleOutline } from 'react-icons/io5'
 
 const Hero = () => {
   return (
-    <div>
-      <div className='bg-heroGreen text-white font-sans w-full flex items-center justify-between p-10 rounded-xl mx-auto my-10 '>
-        <div className='w-1/2'>
-          <h2 className='text-5xl font-bold leading-tight'>
-            Welcome to Lean Recipes, Your Ultimate Guide to Healthy Eating
-          </h2>
-
-          <p className='text-xs mt-4'>
-            From keto to vegan, plaeo to gluten-free, our curated collection of
-            recipes ensures you never have to compromise on on taste or
-            nutrition.
-          </p>
-          <button className='bg-white text-heroGreen py-2 px-4 rounded-lg mt-4 font-light'>
-            Explore Recipes{' '}
-            <IoArrowForwardCircleOutline className='inline-block ml-4' />
-          </button>
-        </div>
-        <div className='w-1/2 relative right-0'>
-          <img src={HeroImg} alt='hero Image' className='object-cover h-1/4' />
-        </div>
+    <div className='relative bg-heroGreen rounded-xl flex items-center justify-between font-sans px-10'>
+      <div className='flex flex-col justify-center h-full w-1/2'>
+        <h1 className='text-white text-5xl font-bold'>
+          Welcome to Lean Recipes, Your Ultimate Guide to Healthy Eating
+        </h1>
+        <p className='text-white font-thin my-5'>
+          From keto to vegan, plaeo to gluten-free, our curated collection of
+          recipes ensures you never have to compromise on on taste or nutrition.
+        </p>
+        <button className='bg-white text-heroGreen py-2 px-4 rounded-lg mt-4 font-light w-1/3'>
+          Explore Recipes{' '}
+          <IoArrowForwardCircleOutline className='inline-block ml-4' />
+        </button>
       </div>
+      <img
+        src={HeroImg}
+        alt='heroImg'
+        className='absolute right-0 z-10 w-1/3 object-cover bottom-0'
+      />
     </div>
   )
 }
