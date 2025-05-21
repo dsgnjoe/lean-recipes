c973b2e843edbc68ac28253e6a2268b318d4f37a
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-21 10:06:48 +0000

    latest recipes api setup

--- src/components/LatestRecipes.jsx
+++ src/components/LatestRecipes.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LatestRecipes = () => {
+  const [loading, setLoading] = useState(true)
+  const [recipes, setRecipes] = useState([])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    fetch('/')
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log('error fetching data', error)
+        setLoading(false)
+      })
+  })
+
   return (
     <div>
       <header className='font-bold text-2xl mb-5'>Latest Recipes</header>
