0bd8f8d7c78b61631b431db730564b241346e78d
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-25 08:12:55 +0000

    recipes ui

--- src/components/LatestRecipes.jsx
+++ src/components/LatestRecipes.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { latestRecipe } from '../data'
+import Spinner from './Spinner'
 
 const LatestRecipes = () => {
   const [loading, setLoading] = useState(true)
@@ -17,6 +18,16 @@ const LatestRecipes = () => {
   return (
     <div className='bg-white w-full mt-28 md:px-20 pt-5'>
       <header className='font-bold text-2xl mb-5'>Latest Recipes</header>
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+          {recipes.map((recipes, index) => (
+            <div key={index} className=''></div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
