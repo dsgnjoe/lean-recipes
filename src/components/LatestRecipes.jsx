44b0116e5d23d77caa352e420962e6ef97ef7799
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-25 08:26:02 +0000

    display recipes

--- src/components/LatestRecipes.jsx
+++ src/components/LatestRecipes.jsx
@@ -24,7 +24,22 @@ const LatestRecipes = () => {
       ) : (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
           {recipes.map((recipes, index) => (
-            <div key={index} className=''></div>
+            <div
+              key={index}
+              className='bg-gray-100 rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300 '
+              onClick={() => navigate(`/recipe/${recipes.id}`)}
+            >
+              <img
+                src={recipes.image}
+                alt={recipes.title}
+                className='w-full h-40 object-cover rounded-t-lg'
+              />
+              <h2 className='text-xl font-semibold mt-3'>{recipes.name}</h2>
+              <p className='text-gray-600 mt-1'>By {recipes.chef}</p>
+              <div>
+                <span className='text-gray-500 text-sm'></span>
+              </div>
+            </div>
           ))}
         </div>
       )}
