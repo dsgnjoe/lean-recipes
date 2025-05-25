2d195892d2b6ee90af0efda8073a4a7e5ec19494
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-25 08:40:12 +0000

    latest recipe ui

--- src/components/LatestRecipes.jsx
+++ src/components/LatestRecipes.jsx
@@ -3,6 +3,8 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { latestRecipe } from '../data'
 import Spinner from './Spinner'
+import { GoClock } from 'react-icons/go'
+import { IoBookmarkOutline } from 'react-icons/io5'
 
 const LatestRecipes = () => {
   const [loading, setLoading] = useState(true)
@@ -29,15 +31,27 @@ const LatestRecipes = () => {
               className='bg-gray-100 rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300 '
               onClick={() => navigate(`/recipe/${recipes.id}`)}
             >
+              {/* recipe image */}
               <img
                 src={recipes.image}
                 alt={recipes.title}
                 className='w-full h-40 object-cover rounded-t-lg'
               />
-              <h2 className='text-xl font-semibold mt-3'>{recipes.name}</h2>
-              <p className='text-gray-600 mt-1'>By {recipes.chef}</p>
-              <div>
-                <span className='text-gray-500 text-sm'></span>
+
+              {/* Recipe data */}
+              <div className='p-4'>
+                <h2 className='text-xl font-semibold mt-3'>{recipes.name}</h2>
+                <p className='text-gray-600 mt-1'>By {recipes.chef}</p>
+
+                <div className='flex justify-between items-center'>
+                  <span className='text-gray-500 text-sm'>
+                    <GoClock className='inline-block' /> {recipes.minutes}{' '}
+                    minutes
+                  </span>
+                  <span>
+                    <IoBookmarkOutline />
+                  </span>
+                </div>
               </div>
             </div>
           ))}
