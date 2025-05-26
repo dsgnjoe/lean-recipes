63e871c4562c221ae7f1751bd83a8eca61395a51
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-26 10:07:45 +0000

    vege tag update

--- src/components/Vegies.jsx
+++ src/components/Vegies.jsx
@@ -1,8 +1,8 @@
 const Vegies = () => {
   return (
     <div>
-      <span className='bg-green-100 text-green-700 p-1 border border-green-300 rounded-md text-sm'>
-        vegetarian
+      <span className='bg-green-50 text-green-600 p-1 border border-green-300 rounded-md text-sm'>
+        Vegetarian
       </span>
     </div>
   )
