221181d6ac551bb68d5c99da869ab12932367597
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-28 09:46:07 +0000

    about img1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,43 @@
 const About = () => {
   return (
-    <div className='px-3 '>
-      <header className='text-center font-hostGrotesk'>
+    <div className='px-3 font-hostGrotesk bg-heroGreenLight'>
+      <header className='text-center'>
         <h1 className='font-bold text-4xl py-2'>About Lean Recipe</h1>
         <span className='text-lg font-thin'>
           Finding & Creating Delicious Recipes Has Never Been Easier! Here Is
           How You Can Make The Most Of The Taste Buds.
         </span>
       </header>
+
+      <section className='my-5 flex flex-col items-center justify-center text-center gap-2'>
+        <div>
+          <h1 className='font-bold text-2xl py-2'>Search & Discover</h1>
+          <p className='font-thin pb-3'>
+            Explore a vast collection of recipes for every meal, diet, and
+            cuisine using smart filters to find recipes by ingredients,
+            difficulty, or cook time.
+          </p>
+          <img src='/img1.png' alt='image' />
+        </div>
+        <div>
+          <h1 className='font-bold text-2xl py-2'>Search & Discover</h1>
+          <p className='font-thin pb-3'>
+            Explore a vast collection of recipes for every meal, diet, and
+            cuisine using smart filters to find recipes by ingredients,
+            difficulty, or cook time.
+          </p>
+          <img src='/img1.png' alt='image' />
+        </div>
+        <div>
+          <h1 className='font-bold text-2xl py-2'>Search & Discover</h1>
+          <p className='font-thin pb-3'>
+            Explore a vast collection of recipes for every meal, diet, and
+            cuisine using smart filters to find recipes by ingredients,
+            difficulty, or cook time.
+          </p>
+          <img src='/img1.png' alt='image' />
+        </div>
+      </section>
     </div>
   )
 }
