d3ff19ad8064e598796e3b8ebd21ea056fe885a6
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-28 09:28:47 +0000

    about page header

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,15 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className='px-3 '>
+      <header className='text-center font-hostGrotesk'>
+        <h1 className='font-bold text-4xl py-2'>About Lean Recipe</h1>
+        <span className='text-lg font-thin'>
+          Finding & Creating Delicious Recipes Has Never Been Easier! Here Is
+          How You Can Make The Most Of The Taste Buds.
+        </span>
+      </header>
+    </div>
+  )
 }
 
 export default About
