ccc0d8f18b942e7e4d7310fbb5f64b73a9c57092
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-05-30 09:42:24 +0000

    signUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+
 const SignUp = () => {
-  return <div>SignUp</div>
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  })
+  const { name, email } = formData
+
+  const onchange = (e) => {
+    const { name, value } = e.target
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  return (
+    <div className=''>
+      <header className='flex flex-col items-center gap-3'>
+        <img src='/logo.png' alt='sign up logo' width={50} />
+        <h1 className='font-hostGrotesk font-bold text-3xl'>Sign up</h1>
+      </header>
+    </div>
+  )
 }
 
 export default SignUp
